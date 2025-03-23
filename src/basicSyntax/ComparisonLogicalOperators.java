d3e2f77c76fe6e656f3c460f445eee4d1e7ad194
yume <masato.umeno.818@gmail.com>
2025-03-23 17:20:11 +0900

    リファクタリング

--- src/basicSyntax/ComparisonLogicalOperators.java
+++ src/basicSyntax/ComparisonLogicalOperators.java
@@ -4,19 +4,19 @@ public class ComparisonLogicalOperators {
 
 	public static void main(String[] args) {
 
-		
+
 		// 1.
 		int a = 1, b = 2;
 		System.out.println(a < b);
-		
+
 		// 2.
 		boolean isSunny = true, isWarm = true;
 		System.out.println(isSunny && isWarm);
-		
+
 		// 3.
 		int x = 1, y = 2;
-		System.out.println(x >= 0 && y%2 == 0 );
-		
+		System.out.println(x >= 0 && y % 2 == 0 );
+
 		// 4.
 		boolean hasPermission = false;
 		System.out.println(!hasPermission);
